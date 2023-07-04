02a21a810f1019c237ab9b8f705894312a549262
August <2819763+Dangku@users.noreply.github.com>
2023-07-04 19:24:39 +0800

    enable adc key wakeup for power on

--- bl30/src_ao/demos/amlogic/n200/t7/bananapi_cm5/keypad.c
+++ bl30/src_ao/demos/amlogic/n200/t7/bananapi_cm5/keypad.c
@@ -9,11 +9,7 @@
 /*KEY ID*/
 #define GPIO_KEY_ID_POWER	GPIOD_3
 
-#define ADC_KEY_ID_MENU		520
-#define ADC_KEY_ID_VOL_DEC	521
-#define ADC_KEY_ID_VOL_INC	522
-#define ADC_KEY_ID_ESC		523
-#define ADC_KEY_ID_HOME		524
+#define ADC_KEY_ID_POWER	520
 
 static void vGpioKeyCallBack(struct xReportEvent event)
 {
@@ -34,9 +30,21 @@ static void vGpioKeyCallBack(struct xReportEvent event)
 
 static void vAdcKeyCallBack(struct xReportEvent event)
 {
+	uint32_t buf[4] = {0};
+
+	switch (event.ulCode) {
+	case ADC_KEY_ID_POWER:
+		buf[0] = POWER_KEY_WAKEUP;
+		STR_Wakeup_src_Queue_Send(buf);
+		break;
+	default:
+		break;
+	}
+
 	printf("ADC key event 0x%x, key code %d, responseTime %d\n",
 		event.event, event.ulCode, event.responseTime);
 }
+
 
 struct xGpioKeyInfo gpioKeyInfo[] = {
 	GPIO_KEY_INFO(GPIO_KEY_ID_POWER, HIGH, EVENT_SHORT,
@@ -44,32 +52,31 @@ struct xGpioKeyInfo gpioKeyInfo[] = {
 };
 
 struct xAdcKeyInfo adcKeyInfo[] = {
-	ADC_KEY_INFO(ADC_KEY_ID_MENU, 0, SARADC_CH2,
-		     EVENT_SHORT,
-		     vAdcKeyCallBack, NULL),
-	ADC_KEY_INFO(ADC_KEY_ID_VOL_DEC, 574, SARADC_CH2,
-		     EVENT_SHORT,
-		     vAdcKeyCallBack, NULL),
-	ADC_KEY_INFO(ADC_KEY_ID_VOL_INC, 1065, SARADC_CH2,
-		     EVENT_SHORT,
-		     vAdcKeyCallBack, NULL),
-	ADC_KEY_INFO(ADC_KEY_ID_ESC, 1557, SARADC_CH2,
-		     EVENT_SHORT | EVENT_LONG,
-		     vAdcKeyCallBack, NULL),
-	ADC_KEY_INFO(ADC_KEY_ID_HOME, 2048, SARADC_CH2,
+	ADC_KEY_INFO(ADC_KEY_ID_POWER, 0, SARADC_CH2,
 		     EVENT_SHORT,
 		     vAdcKeyCallBack, NULL)
 };
 
 void vKeyPadInit(void)
 {
+	/* gpio key */
 	vCreateGpioKey(gpioKeyInfo,
 			sizeof(gpioKeyInfo)/sizeof(struct xGpioKeyInfo));
 	vGpioKeyEnable();
+
+	/* adc key */
+	vCreateAdcKey(adcKeyInfo,
+			sizeof(adcKeyInfo)/sizeof(struct xAdcKeyInfo));
+	vAdcKeyEnable();
 }
 
 void vKeyPadDeinit(void)
 {
+	/* gpio key */
 	vGpioKeyDisable();
 	vDestroyGpioKey();
+
+	/* adc key */
+	vAdcKeyDisable();
+	vDestroyAdcKey();
 }
